acc29e22575068514c2d622418abcb58e25f00ce
Darya <iontseva.dasha>
2023-02-14 19:00:12 +0300

    commit

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumNumbers(int start, int finish)
+{
+    if (start == 0 && finish == 0)
+    {
+        return 0;
+    }
+    else if (start == finish)
+    {
+        return start;
+    }
+    else if (start > finish)
+    {
+        return -1;
+    }
+    return start + SumNumbers(start + 1, finish);
+}
+
+System.Console.Write(SumNumbers(1, 3));
